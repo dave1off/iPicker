0655b54724232618d0b9ccc5d65ef0c15b7d7b52
apple <apple@Mac-mini-apple.local>
2017-08-17 22:51:38 +0200

    Initial Commit

---
+++ ObjCProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCProject
+//
+//  Created by apple on 17.08.17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCProject
+//
+//  Created by apple on 17.08.17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
